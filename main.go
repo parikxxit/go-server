385c29488f8f8ed508a6d6ad046a7545237a6b4c
sirBoastAlot <singhp154154@gmail.com>
2022-04-04 10:16:45 +0530

    added basic webserver in go

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandle)
+	http.HandleFunc("/hello", helloHandle)
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Error running server %v", err)
+	}
+}
+
+func helloHandle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+	}
+	fmt.Fprintf(w, "Hello")
+}
+
+func formHandle(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error while processing form r.ParseForm(): %v", err)
+	}
+	fmt.Println("Post request made")
+	name := r.FormValue("name")
+	add := r.FormValue("address")
+	fmt.Fprintf(w, "Name got is %s with address as %s\n", name, add)
 }
